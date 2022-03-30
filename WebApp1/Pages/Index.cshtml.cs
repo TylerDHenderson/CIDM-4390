3fd5eff5ec2e1d16b52e981854cbd2f412bded88
Sang Ci <sci1@buffs.wtamu.edu>
2022-03-30 13:00:16 +0000

    Updated Index.cshtml.cs

--- WebApp1/Pages/Index.cshtml.cs
+++ WebApp1/Pages/Index.cshtml.cs
@@ -1,19 +1,78 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+public class IndexModel : PageModel
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;        // Required using directives for Data Validation
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApp1.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    [BindProperty]
+        // Add Data Validation Rules
+        [Display(Name = "First Name")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string FirstName {get; set;}
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
+        [BindProperty]
+        public string LastName {get; set;}
+
+        [BindProperty]
+        // Add Data Validation Rules
+        [Display(Name = "Course")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string CouseName {get; set;}
+
+        [BindProperty]
+        public string Semester {get; set;}
+
+        [BindProperty]
+        public int Year {get; set;}
+
+        [BindProperty]
+        public string CourseCoordinator {get;set;}
+        [BindProperty]
+        public string CatalogDescription {get;set;}
+        [BindProperty]
+        public string GradeDistrubution {get;set;}
+        [BindProperty]
+        public string ModificationsCourse {get;set;}
+        [BindProperty]
+        public string StudentFeedback {get;set;}
+        [BindProperty]
+        public string Reflection {get;set;}
+        [BindProperty]
+        public string CourseImprovement {get;set;}
+
+
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+                
+        }
+
+        public void OnPost()
+        {
+            // Print out FirstName and LastName just like any other property.
+            _logger.LogWarning($"On Post {FirstName} {LastName}");
+        }
     }
 
-    public void OnGet()
-    {
 
-    }
-}
+
