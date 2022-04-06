f52f8bfe90a89a3b96853bca3a2a44b1f9dba471
Tyler Henderson <tdhenderson1@buffs.wtamu.edu>
2022-04-06 12:16:10 -0500

    Testing Update

--- WebApp1/Models/Instructor.cs
+++ WebApp1/Models/Instructor.cs
@@ -6,6 +6,7 @@ namespace WebApp1.Models
         public string FirstName {get;set;}
         public string LastName {get;set;}
         public int CourseID {get;set;}
+        public bool FormSubmission {get; set;}
 
 
     }
---
+++ webapp.Tests/ProgramAdminTest.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using WebApp1.Data;
+
+namespace webapp.Tests;
+
+public class ProgramAdminTest
+{
+        [Fact]
+        public async Task AddProgramAdminAsync_ProgramAdminIsAdded()
+        {
+            using (var db = new ApbetProjectContext(Utilities.TestDbContextOptions()))
+            {
+                // Arrange
+                var recId = 10;
+                var expectedProgramAdmin = new ProgramAdmin() 
+                { 
+                    Id = recId, 
+                    FirstName = "Tester",
+                    LastName = "Test",
+                };
+
+                // Act
+                await db.AddProgramAdminAsync(expectedProgramAdmin);
+
+                // Assert
+                var actualProgramAdmin = await db.FindAsync<ProgramAdmin>(recId);
+                Assert.Equal(expectedProgramAdmin, actualProgramAdmin);
+            }
+        }
+
+        [Fact]
+        public async Task SubmitForm()
+        {
+            using (var db = new ApbetProjectContext(Utilities.TestDbContextOptions()))
+            {
+                
+                var expectedFormSubmission = new FormSubmission();
+                bool result = expectedFormSubmission(0);
+
+                Assert.False(result, "Form was not Submitted");
+            }
+        }
+
+        [Fact]
+        public async Task InstructorAuthenticated()
+        {
+            using (var db = new ApbetProjectContext(Utilities.TestDbContextOptions()))
+            {
+                var InstrucID =10;
+                var expectedInstructor = new Instructor()
+                {
+                    InstructorID = InstrucID,
+                    FirstName = "Testing",
+                    LastName = "StillTesting"
+                };
+
+                await db.AddInstructorAuthenticated(expectedInstructor);
+
+                var actualInstructor = await.db.FindAsync<Instructor>(InstrucID);
+                Assert.Equal(expectedInstructor, actualInstructor);
+            }
+        }
+}
+
